3d3f3369bff640823ba4aac0f7279cfef78b2411
DataQueue Systems <dataqueuesystems@DataQueues-MacBook-Air.local>
2024-11-22 18:17:48 +0530

    MacPush22-11

--- src/Screen/User/UserHome.js
+++ src/Screen/User/UserHome.js
@@ -26,10 +26,8 @@ export default function UserHome() {
   const showMyDetail = () => {
     bottomSheetRef2.current?.expand(); // Use expand instead of open
   };
-
   const backPressedOnce = useRef(false);
   const isFocused = useIsFocused();
-
   useEffect(() => {
     const backHandler = BackHandler.addEventListener(
       'hardwareBackPress',
@@ -52,10 +50,10 @@ export default function UserHome() {
     );
 
     return () => backHandler.remove();
-  }, [isFocused]);
+  }, [isFocused]) ;
 
   return (
-    
+
     <>
       <View
         style={[
